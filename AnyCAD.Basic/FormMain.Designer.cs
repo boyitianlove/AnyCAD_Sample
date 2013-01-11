f7eb127bcea729033b6caa3f7cf9dd75cb4a9d79
anycad <anycad@126.com>
2013-01-11 14:18:18 +0800

    Add Basic Sample

---
+++ AnyCAD.Basic/FormMain.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace AnyCAD.Basic
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.primitiveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sphereToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel3d = new System.Windows.Forms.Panel();
+            this.timerDraw = new System.Windows.Forms.Timer(this.components);
+            this.boxToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cylinderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.coneToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.featureToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.extrudeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.revoleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.primitiveToolStripMenuItem,
+            this.featureToolStripMenuItem,
+            this.clearToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(601, 25);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // primitiveToolStripMenuItem
+            // 
+            this.primitiveToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sphereToolStripMenuItem,
+            this.boxToolStripMenuItem,
+            this.cylinderToolStripMenuItem,
+            this.coneToolStripMenuItem});
+            this.primitiveToolStripMenuItem.Name = "primitiveToolStripMenuItem";
+            this.primitiveToolStripMenuItem.Size = new System.Drawing.Size(69, 21);
+            this.primitiveToolStripMenuItem.Text = "Primitive";
+            // 
+            // sphereToolStripMenuItem
+            // 
+            this.sphereToolStripMenuItem.Name = "sphereToolStripMenuItem";
+            this.sphereToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.sphereToolStripMenuItem.Text = "Sphere";
+            this.sphereToolStripMenuItem.Click += new System.EventHandler(this.sphereToolStripMenuItem_Click);
+            // 
+            // panel3d
+            // 
+            this.panel3d.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3d.Location = new System.Drawing.Point(0, 25);
+            this.panel3d.Name = "panel3d";
+            this.panel3d.Size = new System.Drawing.Size(601, 428);
+            this.panel3d.TabIndex = 1;
+            this.panel3d.Paint += new System.Windows.Forms.PaintEventHandler(this.panel3d_Paint);
+            // 
+            // timerDraw
+            // 
+            this.timerDraw.Tick += new System.EventHandler(this.timerDraw_Tick);
+            // 
+            // boxToolStripMenuItem
+            // 
+            this.boxToolStripMenuItem.Name = "boxToolStripMenuItem";
+            this.boxToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.boxToolStripMenuItem.Text = "Box";
+            this.boxToolStripMenuItem.Click += new System.EventHandler(this.boxToolStripMenuItem_Click);
+            // 
+            // cylinderToolStripMenuItem
+            // 
+            this.cylinderToolStripMenuItem.Name = "cylinderToolStripMenuItem";
+            this.cylinderToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.cylinderToolStripMenuItem.Text = "Cylinder";
+            this.cylinderToolStripMenuItem.Click += new System.EventHandler(this.cylinderToolStripMenuItem_Click);
+            // 
+            // coneToolStripMenuItem
+            // 
+            this.coneToolStripMenuItem.Name = "coneToolStripMenuItem";
+            this.coneToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.coneToolStripMenuItem.Text = "Cone";
+            this.coneToolStripMenuItem.Click += new System.EventHandler(this.coneToolStripMenuItem_Click);
+            // 
+            // featureToolStripMenuItem
+            // 
+            this.featureToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.extrudeToolStripMenuItem,
+            this.revoleToolStripMenuItem});
+            this.featureToolStripMenuItem.Name = "featureToolStripMenuItem";
+            this.featureToolStripMenuItem.Size = new System.Drawing.Size(63, 21);
+            this.featureToolStripMenuItem.Text = "Feature";
+            // 
+            // extrudeToolStripMenuItem
+            // 
+            this.extrudeToolStripMenuItem.Name = "extrudeToolStripMenuItem";
+            this.extrudeToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.extrudeToolStripMenuItem.Text = "Extrude";
+            this.extrudeToolStripMenuItem.Click += new System.EventHandler(this.extrudeToolStripMenuItem_Click);
+            // 
+            // revoleToolStripMenuItem
+            // 
+            this.revoleToolStripMenuItem.Name = "revoleToolStripMenuItem";
+            this.revoleToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.revoleToolStripMenuItem.Text = "Revole";
+            this.revoleToolStripMenuItem.Click += new System.EventHandler(this.revoleToolStripMenuItem_Click);
+            // 
+            // clearToolStripMenuItem
+            // 
+            this.clearToolStripMenuItem.Name = "clearToolStripMenuItem";
+            this.clearToolStripMenuItem.Size = new System.Drawing.Size(50, 21);
+            this.clearToolStripMenuItem.Text = "Clear";
+            this.clearToolStripMenuItem.Click += new System.EventHandler(this.clearToolStripMenuItem_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(601, 453);
+            this.Controls.Add(this.panel3d);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FormMain";
+            this.Text = "AnyCAD.Basic";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem primitiveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sphereToolStripMenuItem;
+        private System.Windows.Forms.Panel panel3d;
+        private System.Windows.Forms.Timer timerDraw;
+        private System.Windows.Forms.ToolStripMenuItem boxToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cylinderToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem coneToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem featureToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem extrudeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem revoleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clearToolStripMenuItem;
+    }
+}
+
---
+++ AnyCAD.Basic/FormMain.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using AnyCAD.Platform;
+
+namespace AnyCAD.Basic
+{
+    public partial class FormMain : Form
+    {
+        Platform.Application theApplication = new Platform.Application();
+        Platform.View theView;
+        BrepTools shapeMaker = new BrepTools();
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            theApplication.LoadPlugins();
+            Size size = panel3d.Size;
+            theView = theApplication.Create3dView("OSG", panel3d.Handle.ToInt32(), size.Width, size.Height);
+            theView.RequestDraw();
+
+            this.timerDraw.Enabled = true;
+        }
+
+        private void panel3d_Paint(object sender, PaintEventArgs e)
+        {
+            if (theView == null)
+                return;
+
+            theView.Redraw();
+        }
+
+        private void timerDraw_Tick(object sender, EventArgs e)
+        {
+            theView.RequestDraw();
+            theView.Redraw();
+        }
+
+        private void ShowTopoShape(TopoShape topoShape, int id)
+        {
+            PrsNodeManager nodeManager = theView.GetNodeManager();
+            Entity entity = new Entity();
+            entity.SetTopoShape(topoShape);
+            PrsNode node = nodeManager.CreateSceneNode(entity, id, false);
+            if (node != null)
+            {
+                nodeManager.AddSceneNode(node);
+            }
+        }
+
+        private void ClearScene()
+        {
+            PrsNodeManager nodeManager = theView.GetNodeManager();
+            nodeManager.RemoveAllSceneNode();
+        }
+
+        private void sphereToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            TopoShape sphere = shapeMaker.MakeSphere(new Vector3(0, 0, 0), 40);
+            ShowTopoShape(sphere, 100);
+        }
+
+        private void boxToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            TopoShape box = shapeMaker.MakeBox(new Vector3(40, -20, 0), new Vector3(0, 0, 1), new Vector3(30, 40, 60));
+            ShowTopoShape(box, 101);
+        }
+
+        private void cylinderToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            TopoShape cylinder = shapeMaker.MakeCylinder(new Vector3(80, 0, 0), new Vector3(0, 0, 1), 20, 100, 315);
+            ShowTopoShape(cylinder, 102);
+        }
+
+        private void coneToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            TopoShape cylinder = shapeMaker.MakeCone(new Vector3(120, 0, 0), new Vector3(0, 0, 1), 20, 100, 40, 315);
+            ShowTopoShape(cylinder, 103);
+        }
+
+        private void extrudeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int size = 20;
+            // Create the outline edge
+            TopoShape arc = shapeMaker.MakeArc3Pts(new Vector3(-size, 0, 0), new Vector3(size, 0, 0), new Vector3(0, size, 0));
+            TopoShape line1 = shapeMaker.MakeLine(new Vector3(-size, -size, 0), new Vector3(-size, 0, 0));
+            TopoShape line2 = shapeMaker.MakeLine(new Vector3(size, -size, 0), new Vector3(size, 0, 0));
+            TopoShape line3 = shapeMaker.MakeLine(new Vector3(-size, -size, 0), new Vector3(size, -size, 0));
+
+            TopoShapeGroup shapeGroup = new TopoShapeGroup();
+            shapeGroup.Add(line1);
+            shapeGroup.Add(arc);
+            shapeGroup.Add(line2);
+            shapeGroup.Add(line3);
+
+            TopoShape wire = shapeMaker.MakeWire(shapeGroup);
+            TopoShape face = shapeMaker.MakeFace(wire);
+
+            // Extrude
+            TopoShape extrude = shapeMaker.Extrude(face, 100, new Vector3(0, 0, 1));
+            ShowTopoShape(extrude, 104);
+        }
+
+        private void revoleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int size = 10;
+            // Create the outline edge
+            TopoShape arc = shapeMaker.MakeArc3Pts(new Vector3(-size, 0, 0), new Vector3(size, 0, 0), new Vector3(0, size, 0));
+            TopoShape line1 = shapeMaker.MakeLine(new Vector3(-size, -size, 0), new Vector3(-size, 0, 0));
+            TopoShape line2 = shapeMaker.MakeLine(new Vector3(size, -size, 0), new Vector3(size, 0, 0));
+            TopoShape line3 = shapeMaker.MakeLine(new Vector3(-size, -size, 0), new Vector3(size, -size, 0));
+
+            TopoShapeGroup shapeGroup = new TopoShapeGroup();
+            shapeGroup.Add(line1);
+            shapeGroup.Add(arc);
+            shapeGroup.Add(line2);
+            shapeGroup.Add(line3);
+
+            TopoShape wire = shapeMaker.MakeWire(shapeGroup);
+
+            TopoShape revole = shapeMaker.Revol(wire, new Vector3(size * 3, 0, 0), new Vector3(0, 1, 0), 145);
+
+            ShowTopoShape(revole, 105);
+        }
+
+        private void clearToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ClearScene();
+        }
+    }
+}
